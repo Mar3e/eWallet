a9e7ce746cd5d4e919caf5d8cb9e5733b018852b
Mareai BinTaleb <102855431+Mar3e@users.noreply.github.com>
2023-09-24 13:23:21 +0300

    implemented validation , moved all functions to VM

--- lib/views/signin_view.dart
+++ lib/views/signin_view.dart
@@ -1,165 +1,141 @@
+import 'package:ewalletapp/views_models/auth_view_model.dart';
+import 'package:ewalletapp/widgets/alert_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../views/signup.dart';
 
 class SignInView extends StatelessWidget {
- 
-   SignInView({Key? key}) : super(key: key);
-
-
- 
-  final _formKey = GlobalKey<FormState>();
+  const SignInView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SafeArea(
-        child: Padding(
-          padding:
-              const EdgeInsets.all(8.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Align(
-                    alignment: Alignment.topRight,
-                    child: Text(
-                      "مرحباً",
-                      style: TextStyle(
-                          fontSize: 48, fontWeight: FontWeight.bold),
-                    )),
-                const Text(
-                  "أدخل بريدك الالكتروني لتسجيل الدخول",
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueGrey),
-                ),
-                const SizedBox(
-                  height: 35.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                          labelText: 'البريد الالكتروني',
-                          border: OutlineInputBorder(),
-                          suffixIcon: Icon(Icons.email_outlined),
+    return Consumer<AuthViewModel>(
+        builder: (_, authViewModel, __) => Scaffold(
+              resizeToAvoidBottomInset: true,
+              body: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    key: authViewModel.formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          "مرحباً",
+                          style: TextStyle(
+                              fontSize: 48, fontWeight: FontWeight.bold),
                         ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "الرجاء ادخل بريدك الإلكتروني";
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                      const SizedBox(height: 16),
-                      TextFormField(
-                        obscureText: _obscureText,
-                        decoration: InputDecoration(
-                          labelText: 'كلمة المرور',
-                          border: const OutlineInputBorder(),
-                          suffixIcon: IconButton(
-                            icon: Icon(_obscureText
-                                ? Icons.visibility
-                                : Icons.visibility_off),
+                        const Text(
+                          "أدخل بريدك الالكتروني لتسجيل الدخول",
+                          style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey),
+                        ),
+                        const SizedBox(height: 25.0),
+                        TextFormField(
+                          controller: authViewModel.emailController,
+                          validator: (value) => authViewModel.emailValidator(),
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: 'البريد الالكتروني',
+                            border: OutlineInputBorder(),
+                            suffixIcon: Icon(Icons.email_outlined),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextFormField(
+                          obscureText: authViewModel.obscureText,
+                          controller: authViewModel.passWordController,
+                          validator: (value) =>
+                              authViewModel.passWordValidator(),
+                          decoration: InputDecoration(
+                            labelText: 'كلمة المرور',
+                            border: const OutlineInputBorder(),
+                            suffixIcon: IconButton(
+                                icon: Icon(authViewModel.obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () =>
+                                    authViewModel.toggleObscureText()),
+                          ),
+                        ),
+                        const SizedBox(height: 4.0),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: TextButton(
                             onPressed: () {
-                              //! setState(() {
-                              //   _obscureText = !_obscureText;
-                              // });
+                              // Handle forgot password logic here
                             },
-                          ),
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "الرجاء ادخل كلمة المرور ";
-                          } else {
-                            return null;
-                          }
-                        },
-                      ),
-                      const SizedBox(height: 4.0),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: TextButton(
-                          onPressed: () {
-                            // Handle forgot password logic here
-                          },
-                          child: const Text(
-                            'هل نسيت كلمة المرور؟',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 16.0,
+                            child: const Text(
+                              'هل نسيت كلمة المرور؟',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16.0,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      ElevatedButton(
-                        onPressed: () {
-                         //ToDO : login logic
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            'تسجيل الدخول',
-                            style: TextStyle(
-                                fontSize: 22.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 30.0,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "لا تملك حساب ؟",
-                            style: TextStyle(
-                              color: Colors.blueGrey,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 17.0,
+                        const SizedBox(height: 16.0),
+                        ElevatedButton(
+                          onPressed: () => authViewModel.signIn().then((value) {
+                            if (!value) {
+                              ShowAlertDialog().showAlert(
+                                  context: context,
+                                  title: "حدث خطأ ما",
+                                  content:
+                                      "لم يتم العثور على المستخدم تأكد من إدخال البيانات بشكل صحيح");
+                            }
+                          }),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'تسجيل الدخول',
+                              style: TextStyle(
+                                  fontSize: 22.0, fontWeight: FontWeight.bold),
                             ),
                           ),
-                          TextButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        const SignUpPage(),
+                        ),
+                        const SizedBox(
+                          height: 30.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "لا تملك حساب ؟",
+                              style: TextStyle(
+                                color: Colors.blueGrey,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 17.0,
+                              ),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const SignUpPage(),
+                                    ),
+                                  );
+                                },
+                                child: const Text(
+                                  "سجل الان",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 17.0,
                                   ),
-                                );
-                              },
-                              child: const Text(
-                                "سجل الان",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 17.0,
-                                ),
-                              ))
-                        ],
-                      ),
-                    ],
+                                ))
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+              ),
+            ));
   }
 }
--- lib/views_models/auth_view_model.dart
+++ lib/views_models/auth_view_model.dart
@@ -2,28 +2,49 @@ import 'package:ewalletapp/Models/user.dart';
 import 'package:ewalletapp/repositories/auth_repo/auth_repository.dart';
 import 'package:flutter/material.dart';
 
-class AuthViewModel extends ChangeNotifier{
-final AuthRepository _authRepository;
+import '../Utilities/validators.dart';
+
+// This view model are used for the both of signIn and signUp views for simplicity
+class AuthViewModel extends ChangeNotifier {
+  final AuthRepository _authRepository;
 
   AuthViewModel(this._authRepository);
 
   User? _currentUser;
   bool _isSigned = false;
+  bool _obscureText = true;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _passWordController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
 
   User? get currentUser => _currentUser;
   bool get isSigned => _isSigned;
+  bool get obscureText => _obscureText;
+  get formKey => _formKey;
+  get passWordController => _passWordController;
+  get emailController => _emailController;
 
+  void toggleObscureText() {
+    _obscureText = !_obscureText;
+    notifyListeners();
+  }
 
-Future<bool> signIn(String email, String password) async {
+  Future<bool> signIn() async {
     try {
-      final user = await _authRepository.getUser(email, password);
-      if (user != null) {
-        _currentUser = user;
-        _isSigned = true;
-        notifyListeners();
-        return true; // Sign-in successful
+      if (formValidator()) {
+        final user = await _authRepository.getUser(
+            _emailController.text, _passWordController.text);
+        if (user != null) {
+          _currentUser = user;
+          _isSigned = true;
+          notifyListeners();
+          return true; // Sign-in successful
+        } else {
+          print("no user found"); // User not found
+          return false;
+        }
       } else {
-        return false; // User not found
+        return true;
       }
     } catch (e) {
       print('Sign-in error: $e');
@@ -31,13 +52,14 @@ Future<bool> signIn(String email, String password) async {
     }
   }
 
-   Future<bool> signUp(String userName, String userEmail, int userPhone, String userPassword) async {
+  Future<bool> signUp(String userName, String userEmail, int userPhone,
+      String userPassword) async {
     try {
-   
-      final newUser = await _authRepository.addUser(userName, userEmail, userPhone, userPassword);
+      final newUser = await _authRepository.addUser(
+          userName, userEmail, userPhone, userPassword);
 
       _currentUser = newUser;
-       _isSigned = true;
+      _isSigned = true;
       notifyListeners();
       return true; // Sign-up successful
     } catch (e) {
@@ -51,5 +73,14 @@ Future<bool> signIn(String email, String password) async {
     _isSigned = false;
     notifyListeners();
   }
-}
 
+  //validators
+
+  String? emailValidator() => validateEmail(_emailController.text);
+  String? passWordValidator() => notEmpty(_passWordController.text);
+
+  bool formValidator() {
+    bool isValid = _formKey.currentState!.validate();
+    return isValid;
+  }
+}
