0d12f3d7de68629457d7adcbf737a46502c54196
Mareai BinTaleb <102855431+Mar3e@users.noreply.github.com>
2023-10-11 09:43:15 +0300

    added spacers

---
+++ lib/Core/Utilities/spacres.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Widget addVerticalSpace(double pixels) {
+  return SizedBox(
+    height: pixels,
+  );
+}
+
+Widget addHorizontalSpace(double pixels) {
+  return SizedBox(
+    width: pixels,
+  );
+}
--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,4 +1,6 @@
 import 'package:ewalletapp/Core/Constants/colors_consts.dart';
+import 'package:ewalletapp/Core/Utilities/sizes_config.dart';
+import 'package:ewalletapp/Core/Utilities/spacres.dart';
 import 'package:ewalletapp/views_models/dash_board_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -12,40 +14,65 @@ class DashBoardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Consumer2<AuthViewModel, DashBoardViewModel>(
-        builder: (_, authViewModel, dashBoardViewModel, __) => Scaffold(
-          key: dashBoardViewModel.scaffoldKey,
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                SizedBox(
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 55,
-                        height: 55,
-                        decoration: ShapeDecoration(
-                          image: const DecorationImage(
-                            image: AssetImage("assets/images/avatar.png"),
-                            fit: BoxFit.fill,
-                          ),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                        ),
-                      ),
-                      Column(
+        builder: (_, authViewModel, dashBoardViewModel, __) => LayoutBuilder(
+          builder: (context, constraints) {
+            SizesConfig().init(constraints);
+            print(
+                "${SizesConfig.widthMultiplier}  ${SizesConfig.heightMultiplier}");
+            return Scaffold(
+              key: dashBoardViewModel.scaffoldKey,
+              body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          const Text('مرحباً'),
-                          Text(authViewModel.currentUser!.userName),
+                          Row(
+                            children: [
+                              Container(
+                                width: 55,
+                                height: 55,
+                                decoration: ShapeDecoration(
+                                  image: const DecorationImage(
+                                    image:
+                                        AssetImage("assets/images/avatar.png"),
+                                    fit: BoxFit.fill,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                ),
+                              ),
+                              addHorizontalSpace(10.0),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text('مرحباً'),
+                                  Text(authViewModel.currentUser!.userName),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Container(
+                            width: 36,
+                            height: 36,
+                            decoration: ShapeDecoration(
+                              color: LightColors.secondaryColor,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8)),
+                            ),
+                            child: Icon(Icons.notifications),
+                          ),
                         ],
                       ),
-                    ],
-                  ),
-                  // subtitleTextStyle: TextStyle(fontSize: 15),
-                )
-              ],
-            ),
-          ),
+                      // subtitleTextStyle: TextStyle(fontSize: 15),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
